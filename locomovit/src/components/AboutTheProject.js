bc3c80c743c4005d747622376e13314d59b05eef
davirany <davirany@gmail.com>
2023-04-29 21:53:41 -0300

    develop About Us page

--- locomovit/src/components/AboutTheProject.js
+++ locomovit/src/components/AboutTheProject.js
@@ -7,7 +7,8 @@ function AboutTheProject() {
         <div className="about-the-project">
             <Header />
             <h2>Sobre o projeto</h2>
-            <p>O projeto é um aplicativo web criado para ajudar as pessoas a gerenciar suas tarefas diárias de forma eficiente.</p>
+            <p>Locomovit é um aplicativo web criado para ajudar você a gerenciar suas tarefas diárias de forma eficiente. Seja a sua ida ao trabalho, seja um compromisso no final de semana. O intuito do Locomovit é fazer você ganhar tempo com o trajeto mais curto para que você chegue ao seu destino final. Com rotas eficientes e inteligentes priorizando você.</p>
+
         </div>
     );
 }
